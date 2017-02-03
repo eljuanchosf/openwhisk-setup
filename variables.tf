03727e70e8e1f156a8a6e8a9e4fc40515b772014
David Benko <DavidBenko@users.noreply.github.com>
2017-02-03 12:45:47 -0500

    remove duplicate prefix

--- variables.tf
+++ variables.tf
@@ -3,7 +3,6 @@ variable "aws_secret_key" {}
 variable "aws_key_path" {}
 variable "aws_key_name" {}
 variable "aws_openwhisk_ami" {}
-variable "prefix" {}
 
 variable "aws_region" {
   default = "us-east-1"
